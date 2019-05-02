f32019fe98837f15d8f218d2f41d5f7fc1a861e0
Hannah Brinsko <h.brinsko@utexas.edu>
2019-05-02 18:21:23 -0500

    adding time saving information

--- distance.py
+++ distance.py
@@ -2,17 +2,38 @@ import googlemaps
 from datetime import datetime
 import numpy as np
 import config
+import json
+import config
 
-gmaps = googlemaps.Client(key=config.google_key)
 
-# # Coverts to latlong
-# geocode_result = gmaps.geocode('1007 S Congress Ave, Austin, TX 78704')
-# print(str(geocode_result))
+def calculate_without_tolls(gmaps, now):
+    without_tolls = gmaps.distance_matrix(config.start_point,
+                                          config.end_point,
+                                          mode="driving",
+                                          avoid="tolls",
+                                          departure_time=now)
 
-now = datetime.now()
-matrix = gmaps.distance_matrix("Round Rock, TX", "Austin, TX",
-                mode="driving",
-                avoid="tolls",
-                departure_time=now,
-                traffic_model="optimistic")
-print(str(matrix))
+    no_toll_drivetime = without_tolls['rows'][0]['elements'][0]['duration']['text']
+    return int(no_toll_drivetime.replace(" mins", ""))
+
+
+def calculate_with_tolls(gmaps, now):
+    with_tolls = gmaps.distance_matrix(config.start_point,
+                                       config.end_point,
+                                       mode="driving",
+                                       departure_time=now)
+    toll_drivetime = with_tolls['rows'][0]['elements'][0]['duration']['text']
+    return int(toll_drivetime.replace(" mins", ""))
+
+
+def calc_time_saved():
+    gmaps = googlemaps.Client(key=config.google_key)
+    now = datetime.now()
+    time_without = calculate_without_tolls(gmaps, now)
+    time_with = calculate_with_tolls(gmaps, now)
+    diff = time_without - time_with
+    return diff
+
+
+if __name__ == '__main__':
+    calc_time_saved()
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import mopac
+import distance
 
 app = Flask(__name__)
 
@@ -9,8 +10,9 @@ def sms_reply():
     resp = MessagingResponse()
     
     current_toll = mopac.get_specific_toll('sb: parmer to 5th/cvz')
+    time_saved = distance.calc_time_saved()
 
-    resp.message("Current SB Mopac Toll is $" + str(current_toll))
+    resp.message("Hey Hannah! Toll rate to get home is $" + str(current_toll) + " and  will save you " + str(time_saved) + " minutes.")
     return str(resp)
 
 if __name__ == "__main__":
