63a952c1a10573da151a813706e8fb20908d19c0
Hannah Brinsko <h.brinsko@utexas.edu>
2019-05-19 20:11:10 -0500

    give northbound vs southbound toll info

--- distance.py
+++ distance.py
@@ -3,37 +3,39 @@ from datetime import datetime
 import numpy as np
 import config
 import json
-import config
+import re
 
 
-def calculate_without_tolls(gmaps, now):
-    without_tolls = gmaps.distance_matrix(config.start_point,
-                                          config.end_point,
+def calculate_without_tolls(gmaps, now, origin, destination):
+    without_tolls = gmaps.distance_matrix(origin,
+                                          destination,
                                           mode="driving",
                                           avoid="tolls",
                                           departure_time=now)
 
     no_toll_drivetime = without_tolls['rows'][0]['elements'][0]['duration']['text']
-    return int(no_toll_drivetime.replace(" mins", ""))
+    no_toll_drivetime  = re.findall("\d+", no_toll_drivetime)
+    return int(no_toll_drivetime[0])
 
 
-def calculate_with_tolls(gmaps, now):
-    with_tolls = gmaps.distance_matrix(config.start_point,
-                                       config.end_point,
+def calculate_with_tolls(gmaps, now, origin, destination):
+    with_tolls = gmaps.distance_matrix(origin,
+                                       destination,
                                        mode="driving",
                                        departure_time=now)
     toll_drivetime = with_tolls['rows'][0]['elements'][0]['duration']['text']
-    return int(toll_drivetime.replace(" mins", ""))
+    toll_drivetime  = re.findall("\d+", toll_drivetime)
+    return int(toll_drivetime[0])
 
 
-def calc_time_saved():
-    gmaps = googlemaps.Client(key=config.google_key)
+def calc_time_saved(origin, destination):
+    gmaps = googlemaps.Client(key=config.GOOGLE_KEY)
     now = datetime.now()
-    time_without = calculate_without_tolls(gmaps, now)
-    time_with = calculate_with_tolls(gmaps, now)
+    time_without = calculate_without_tolls(gmaps, now, origin, destination)
+    time_with = calculate_with_tolls(gmaps, now, origin, destination)
     diff = time_without - time_with
     return str(diff)
 
 
 if __name__ == '__main__':
-    calc_time_saved()
+    calc_time_saved(config.WORK_ADDRESS, config.HOME_ADDRESS)
--- run.py
+++ run.py
@@ -2,16 +2,27 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import mopac
 import distance
+import config
 
 app = Flask(__name__)
 
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
+    body = request.values.get('Body', None)
     resp = MessagingResponse()
 
-    current_toll = mopac.get_specific_toll('sb: parmer to 5th/cvz')
-    time_saved = distance.calc_time_saved()
+    if body == 'nb':
+        current_toll = mopac.get_specific_toll('nb: cvz to 183')
+        time_saved = distance.calc_time_saved(
+        config.HOME_ADDRESS, config.WORK_ADDRESS)
+    elif body == 'sb':
+        current_toll = mopac.get_specific_toll('sb: 2222 to 5th/cvz')
+        time_saved = distance.calc_time_saved(
+        config.WORK_ADDRESS, config.HOME_ADDRESS)
+    else:
+        resp.message('Please reply which direction (nb/sb) you are heading.')
+        return(str(resp))
 
     if(time_saved == '0'):
         msg = "Don't take the toll, it won't save you any time and will cost you " + \
